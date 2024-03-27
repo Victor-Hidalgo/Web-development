cca569daaa37593ef260a0928be1ee20392671d1
Victor Hidalgo <victorhidalgorivas@gmail.com>
2024-03-27 12:01:58 +0000

    First commit

---
+++ myproject/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <StatusBar style="auto" />
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum <Text>testing</Text> dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor:'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    fontWeight: 'bold',
+  }
+});
