563258a3b8f2f25e6bab08f3963daea81f9dae9e
Victor Hidalgo <victorhidalgorivas@gmail.com>
2024-03-30 18:28:20 +0000

    Fourth commit

--- myproject/App.js
+++ myproject/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -13,6 +13,13 @@ export default function App() {
     { name: 'daisy', id: '8' },
   ]);
 
+  const pressHandler = (id) => {
+    console.log(id)
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id)
+    })
+  }
+
   return (
     <View style={styles.container}>
 
@@ -21,17 +28,11 @@ export default function App() {
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
-
-    {/* <ScrollView>
-      { people.map(item => (
-        <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-        </View>
-        ))}
-    </ScrollView> */}
 
     </View>
   );
