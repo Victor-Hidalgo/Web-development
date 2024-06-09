50d9799f38a96c9cf341981edcf8b7f612ec5278
Victor Hidalgo <victorhidalgorivas@gmail.com>
2024-06-09 13:36:31 +0100

    adjusted Generics file

--- 02setup/src/myGenerics.ts
+++ 02setup/src/myGenerics.ts
@@ -2,15 +2,15 @@ const score: Array<number> = []
 const names: Array<string> = []
 
 function identityOne(val: boolean | number): boolean | number {
-    return val
+    return val;
 }
 
 function identityTwo(val: any): any {
-    return val
+    return val;
 }
 
 function identityThree<Type>(val: Type): Type {
-    return val
+    return val;
 }
 
 // identityThree(true)
@@ -24,4 +24,48 @@ interface Bottle {
     type: number
 }
 
-identityFour<Bottle>({})
+function getSearchProducts<T>(products: T[]): T {
+    // do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+
+const getMoreProducts = <T,>(products: T[]): T => {
+    // do some database operations
+    const myIndex = 4
+    return products[myIndex]
+}
+
+interface Database {
+    connection: string,
+    username: string,
+    password: string
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
+    }
+}
+
+// anotherFunction(3, {})
+
+interface Quiz {
+    name: string,
+    type: string
+}
+
+interface Course {
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T>{
+    public cart: T[] = []
+
+    addToCart(product: T) {
+        this.cart.push(product)
+    }
+}
